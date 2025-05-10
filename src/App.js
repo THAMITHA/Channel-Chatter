00b238546bf5da276bb8091d6de5fbca5e5934bc
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-10 23:06:46 +0530

    update reduxstate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+import { Provider } from "react-redux";
 import "./App.css"
 import Body from "./components/Body";
 import Head from "./components/Head";
+import store from "./utils/store";
 
 function App() {
   return (
-    <div>
-      <Head/>
-      <Body/>
-      {/**
-       * 
-       * Head
-       * Body
-       * Sidebar
-       *  MenuItems
-       * MainContainer
-       *  ButtonList
-       *  VideoContainer
-       *    VideoCard
-       *  
-       * 
-       */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <Head/>
+        <Body/>
+        {/**
+         * 
+         * Head
+         * Body
+         * Sidebar
+         *  MenuItems
+         * MainContainer
+         *  ButtonList
+         *  VideoContainer
+         *    VideoCard
+         *  
+         * 
+         */}
+      </div>
+    </Provider>
   );
 }
 
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { toggleMenu } from '../utils/appSlice';
 
 const Head = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = ()=>{
+    dispatch(toggleMenu())
+  }
   return (
-    <div>Head</div>
+    <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
+      <div className='flex col-span-1'>
+        <img onClick={()=>toggleMenuHandler()} className="h-8 cursor-pointer" alt="menu" src="https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/What%20is%20a%20Hamburger%20Button.png?width=450&name=What%20is%20a%20Hamburger%20Button.png"/>
+        <a href="/">
+          <img className="h-8" alt="youtube-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1200px-YouTube_Logo_2017.svg.png"/>
+        </a>
+      </div>
+    <div className='col-span-10 px-10'>
+      <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text"/>
+      <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+        🔍
+      </button>
+    </div>
+    <div className='col-1'>
+      <img className="h-12"
+      alt="user" 
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoB4l1vBNqbEpwil6Jsrpr6gE8wLY5_23geyqPK0QyhVo9x24z31MDvpC1WvDF3Gp8h6o&usqp=CAU"/>
+    </div>
+    </div>
+    
   )
 }
 
