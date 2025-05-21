aa856a006536c68a456aeb5588c44db71a40369b
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-21 05:51:59 +0530

    added chat message

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -6,7 +6,7 @@ export const ChatMessage = ({name, message}) => {
          <img className="h-12"
       alt="user" 
       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoB4l1vBNqbEpwil6Jsrpr6gE8wLY5_23geyqPK0QyhVo9x24z31MDvpC1WvDF3Gp8h6o&usqp=CAU"/>
-      <span className='font-bold px-4'>{name}</span>  
+      <span className='font-bold px-2'>{name}</span>  
       <span>{message}</span>
     </div>
   )
--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -4,7 +4,7 @@ import { ChatMessage } from './ChatMessage'
 const LiveChat = () => {
   return (
     <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
-      <ChatMessage name="thamitha" message="This is Namasate React live! 🙏" />
+      <ChatMessage name="Thamitha" message="This is Namasate React live! 🙏" />
     </div>
   )
 }
