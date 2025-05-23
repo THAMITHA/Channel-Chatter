9435226519a5088ae8eece3cb384da4f0a78f2c0
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-23 21:15:36 +0530

    added changes

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ChatMessage = ({name, message}) => {
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center shadow-sm p-2'>
          <img className="h-12"
       alt="user" 
       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoB4l1vBNqbEpwil6Jsrpr6gE8wLY5_23geyqPK0QyhVo9x24z31MDvpC1WvDF3Gp8h6o&usqp=CAU"/>
--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -5,6 +5,9 @@ const LiveChat = () => {
   return (
     <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
       <ChatMessage name="Thamitha" message="This is Namasate React live! 🙏" />
+      <ChatMessage name="Thamitha" message="This is Namasate React live! 🙏" />
+      <ChatMessage name="Thamitha" message="This is Namasate React live! 🙏" />
+      <ChatMessage name="Thamitha" message="This is Namasate React live! 🙏" />
     </div>
   )
 }
