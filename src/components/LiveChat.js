91a1015e6593fad9db3e2241271e23b846063360
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-24 22:01:02 +0530

    update redux store

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { ChatMessage } from './ChatMessage'
 import { useDispatch, useSelector } from 'react-redux'
 import { addMessage } from '../utils/chatSlice';
-
+import { generateRandomName, makeRandomMessage } from '../utils/helper';
 const LiveChat = () => {
   const dispatch = useDispatch();
   const chatMessages = useSelector((store)=> store.chat.messages);
@@ -10,14 +10,14 @@ const LiveChat = () => {
     const i = setInterval(()=>{
       console.log("API Polling");
       dispatch(addMessage({
-        name: "Thamitha Rajput",
-        message: "hello thamitha don't worry things will get sorted 🚀"
+        name: generateRandomName(),
+        message: makeRandomMessage(20)+ "🚀"
       }))
     }, 2000);
     return () => clearInterval(i);
   }, [])
   return (
-    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll'>
+    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse'>
      { //Disclaimer: Don't use indexes as keys
      chatMessages.map((c, i)=><ChatMessage key={i} name={c.name} message={c.message} />)}
     </div>
