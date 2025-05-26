2fd19942daf77c4b4b656691d5752e610bb49943
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-26 22:05:07 +0530

    update backgroundcolor

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -3,10 +3,11 @@ import { findPrime } from '../utils/helper'
 
 export const Demo = () => {
     const [text, setText] = useState(0)
+    const [isDarkTheme, setIsDarkTheme] = useState(true)
     console.log('Rendering...')
     const prime = findPrime(text) 
   return (
-    <div className='m-4 p-2 w-96 h-96 border border-black'>
+    <div className={"m-4 p-2 w-96 h-96 border border-black "+ (isDarkTheme && "bg-gray-900")}>
         <div>
             <input className="border border-black w-72 px-2" type='number' value={text} onChange={(e)=>setText(e.target.value)}/>
         </div>
