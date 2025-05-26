803817fb0b91846a2966ca17b15e2cd19162887a
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-27 01:20:15 +0530

    useState use normal let

--- src/components/Demo2.js
+++ src/components/Demo2.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const Demo2 = () => {
     const [y, setY] = useState(10)
-    let x = 10;
+    let x = 0;
     
   return (
     <div className='m-4 p-2 bg-slate-50 border border-black w-96 h-96'>
@@ -11,7 +11,7 @@ const Demo2 = () => {
         <button className="bg-green-100 px-2 m-4"
          onClick={()=>{
                 x=x+1;
-                console.log(x)
+                console.log("x= "+ x)
             }}>Increase x</button>
         <span className='font-bold text-xl'>Let = {x}</span>
         
